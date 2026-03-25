6a58c6444510df96e07f85fa2d9efdcc40bece54
Ayush-cmd-sudo <ayu021singh@gmail.com>
2026-03-25 13:37:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,54 @@
 //complete this code
 class Person {
-constructor(name,age){
-	this._name=name;
-	this._age=age;
-}
-get name(){
-	return this._name;
-}	
-set age(value){
-	if(value<=0){
-		console.log("Invalid age");
-	}
-    else{
-		this._age=value;
-
-	}
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
   }
-get age(){
-	return this._age;
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for age
+  set age(newAge) {
+    this._age = newAge;
+  }
+
+  // Getter for age (needed to read updated value)
+  get age() {
+    return this._age;
+  }
 }
 
-
-}
-
+// Student Class
 class Student extends Person {
- super();
-  study(){
-	  console.log(this._name,"is studying.");
+  study() {
+    console.log(`${this.name} is studying`);
   }
-	
 }
 
+// Teacher Class
 class Teacher extends Person {
-super();
-	teach(){
-		console.log(this._name,"is teaching");
-	}
-	
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
+
+
+// Example Usage
+
+const person = new Person("John", 25);
+console.log(person.name); // John
+
+person.age = 30;
+console.log(person.age); // 30
+
+const student = new Student("Alice", 22);
+student.study(); // Alice is studying
+
+const teacher = new Teacher("Bob", 40);
+teacher.teach(); // Bob is teaching
 
 // Do not change the code below this line
 window.Person = Person;
