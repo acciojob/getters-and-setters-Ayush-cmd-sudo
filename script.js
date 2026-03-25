b26a7e2f09663fe2d6f1394ed27fb68872f1eb40
Ayush-cmd-sudo <ayu021singh@gmail.com>
2026-03-25 13:32:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
 //complete this code
-class Person {}
+class Person {
+constructor(name,age){
+	this._name=name;
+	this._age=age;
+}
+get name(){
+	return this._name;
+}	
+set age(value){
+	if(value<=0){
+		console.log("Invalid age");
+	}
+    else{
+		this._age=value;
 
-class Student extends Person {}
+	}
+  }
+get age(){
+	return this._age;
+}
 
-class Teacher extends Person {}
+
+}
+
+class Student extends Person {
+ super();
+  study(){
+	  console.log(this._name,"is studying.");
+  }
+	
+}
+
+class Teacher extends Person {
+super();
+	teach(){
+		console.log(this._name,"is teaching");
+	}
+	
+}
 
 // Do not change the code below this line
 window.Person = Person;
